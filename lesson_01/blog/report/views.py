90fe0494cbcf2e2f3257e32ad911909e5daddb6d
Tatyana Koromyslova <lumpik063@mail.ru>
2023-05-12 14:10:25 +0100

    home work 4

--- lesson_01/blog/report/views.py
+++ lesson_01/blog/report/views.py
@@ -1,16 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import logout_user, login_user, login_required, current_user
+from werkzeug.security import check_password_hash
 
-report = Blueprint('report', __name__, url_prefix='/reports', static_folder='../static')
+from blog.models import User
+
+auth = Blueprint('auth', __name__, static_folder='../static')
 
 
-@report.route('/')
-def report_list():
-    return 'Hello report'
+@auth.route('/login', methods=('GET',))
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('user.profile', pk=current_user.id))
 
-
-@report.route("/")
-def report_list():
     return render_template(
-        "base.html",
-        reports=[1,231,4]
+        'auth/login.html',
+    )
+
+
+@auth.route('/login', methods=('POST',))
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        flash('Check your login details')
+        return redirect(url_for('.login'))
+
+    login_user(user)
+    return redirect(url_for('user.profile', pk=user.id))
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('.login'))
     )
