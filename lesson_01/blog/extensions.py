d6ac57199d0503fe3b771359405c9b741f4ec23b
Tatyana Koromyslova <lumpik063@mail.ru>
2023-06-02 14:35:22 +0100

    home work 8

--- lesson_01/blog/extensions.py
+++ lesson_01/blog/extensions.py
@@ -3,7 +3,15 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
 
+from blog.admin.views import CustomAdminIndexView
+
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 csrf = CSRFProtect()
+admin = Admin(
+    index_view=CustomAdminIndexView(),
+    name='Blog Admin Panel',
+    template_mode='bootstrap4',
+)
+
